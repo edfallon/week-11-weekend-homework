2daf24a1b854f90d7db09fffcdf93f90e108ed74
Edward Fallon <eddie_fallon@hotmail.com>
2018-06-09 23:39:21 +0100

    passing test for collector buying record

--- record_collector.js
+++ record_collector.js
@@ -3,4 +3,11 @@ const RecordCollector = function(cash){
   this.collection = [];
 };
 
+RecordCollector.prototype.buyRecord = function (record) {
+  if(this.cash >= record.price){
+    this.cash - record.price
+    this.collection.push(record);
+  }
+};
+
 module.exports = RecordCollector;
--- specs/record_collector_spec.js
+++ specs/record_collector_spec.js
@@ -1,15 +1,17 @@
 const assert = require('assert');
 const RecordCollector = require('../record_collector.js');
-
+const Record = require('../record.js');
 
 describe('recordCollector', function(){
 
   let recordCollector;
+  let record;
 
 
 
     beforeEach(function(){
         recordCollector1 = new RecordCollector(100);
+        record2 = new Record("Eminem", "The Marshall Mathers LP", "Rap", 10);
       });
 
   it('should have a cash', function(){
@@ -18,6 +20,11 @@ describe('recordCollector', function(){
 
   it('should have a collection of records that starts empty', function(){
     assert.strictEqual(recordCollector1.collection.length, 0);
+  });
+
+  it('should be able to buy record if they have enough money', function(){
+    recordCollector1.buyRecord(record2)
+    assert.strictEqual(recordCollector1.collection.length, 1);
   });
 
 
