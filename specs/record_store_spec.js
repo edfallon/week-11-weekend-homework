faf0e74cbbb2208f0748f00d87f0ef94c03d4ac4
Edward Fallon <eddie_fallon@hotmail.com>
2018-06-09 16:01:24 +0100

    passing test for store balance

--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -7,7 +7,7 @@ describe('recordStore', function(){
 
 
     beforeEach(function(){
-        recordStore = new RecordStore("Sifter's", "Manchester");
+        recordStore = new RecordStore("Sifter's", "Manchester", 1000);
       });
 
   it('should have a name', function(){
@@ -20,6 +20,10 @@ describe('recordStore', function(){
 
   it('should have an inventory that starts empty', function(){
     assert.strictEqual(recordStore.inventory.length, 0);
+  });
+
+  it('should have a balance', function(){
+    assert.strictEqual(recordStore.balance, 1000);
   });
 
 
